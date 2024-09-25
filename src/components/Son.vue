2bcc14f820a37d6d11e1d1c902b8f8e700bba3c9
DianWoung <dian126@foxmail.com>
2024-09-25 23:57:49 +0800

    init

---
+++ src/components/Son.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>My Num: {{ msg }}</h1>
+</template>
+
+<script setup>
+import {watch} from 'vue'
+
+const props = defineProps({
+  msg: {
+    type: Number,
+    required: true
+  }
+})
+watch(props, (newX) => {
+  console.log(`x is ${newX.msg}`)
+})
+</script>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button @click="parentMsg++">数字追加器</button>
+    <Son :msg="parentMsg" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Son from '../components/Son.vue'
+const parentMsg = ref(1)
+</script>
+
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
